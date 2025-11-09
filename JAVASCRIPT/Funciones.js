4476ad960d986394405c712f861620f85c237fe8
EXBSON JAIR MENDEZ PARRA <exbsonmendez@gmail.com>
2025-11-09 16:34:21 -0500

    Summary index.hmtl
    actualización en índex, jaca script y imágenes

--- JAVASCRIPT/Funciones.js
+++ JAVASCRIPT/Funciones.js
@@ -1,9 +1,21 @@
-function registador() {
-  const nombre = document.getElementById("nombre").value;
-  const email = document.getElementById("email").value;
+/* function Registrado() {
+  const nombre = document.getElementById("Nombre").value;
+  const apellido = document.getElementById("Apellido").value;
+  const email = document.getElementById("Correo").value;
+  const teléfono = document.getElementById("Teléfono").value;
+  const empresa = document.getElementById("Empresa").value;
+  const comentario = document.getElementById("mensaje").value;
 
+  console.log("Informacion del Cliente:");
   console.log("Nombre:", nombre);
-  console.log("Email:", email);
+  console.log("Apellido:", apellido);
+  console.log("Correo:", email);
+  console.log("Telefono:", teléfono);
+  console.log("Empresa:", empresa);
+  console.log("Comentario:", comentario);
 
-  alert(`Datos enviados:\nNombre: ${nombre}\nEmail: ${email}`);
-}
+  alert(
+    `Datos enviados:\nNombre: ${nombre}\nApellido: ${apellido}\nCorreo: ${email}\nTelefono: ${teléfono}
+    \nEmpresa: ${empresa}\nComentario: ${comentario}`
+  );
+} */
